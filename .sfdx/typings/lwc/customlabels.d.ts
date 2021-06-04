7a0d1fcf2d1074c973aa0cc1763cd985777cc283
Geetika Kagdiyal <geetika.kagdiyal@nagarro.com>
2021-06-04 18:05:54 +0530

    initial commit

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/label/c.New_Custom_label" {
+    var New_Custom_label: string;
+    export default New_Custom_label;
+}
+declare module "@salesforce/label/c.New_Custom_label_2" {
+    var New_Custom_label_2: string;
+    export default New_Custom_label_2;
+}
